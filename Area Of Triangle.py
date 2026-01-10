9e58563e9896c5125342f1b9be09c19f57ef735c
bharani1829 <orsuleelabharani18@gmail.com>
2026-01-10 11:47:10 +0530

    Add files via upload

---
+++ Area Of Triangle.py
@@ -0,0 +1,6 @@
+#area of rectangle 
+#Formula = Length x width
+length = float(input("Enter the length :"))
+width = float(input("Enter the width :"))
+Area = length * width
+print(Area)
---
+++ Shopping Cart.py
@@ -0,0 +1,7 @@
+#shopping Cart
+item = input("What item would you like to buy ?:")
+price = float(input("What is The price ?: "))
+quantity = int(input("How many ?: "))
+total = price * quantity
+print(f"Youe item is {item} And you want {quantity}{item} ")
+print(f"Total cost id {total}")
