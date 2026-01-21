564c70c4af57e57fd425e48c0810c15d2b87a328
bharani1829 <orsuleelabharani18@gmail.com>
2026-01-21 12:31:04 +0000

    Update List.py

---
+++ List.py
@@ -0,0 +1,8 @@
+#WAP to ask the user to enter names of their 3 favorite movies & store them in a list
+list = []
+
+list.append(input("Enter 1st Favorite Movie : "))
+list.append(input("Enter 2nd Favorite Movie : "))
+list.append(input("Enter 3rd Favorite Movie : "))
+
+print(list)
